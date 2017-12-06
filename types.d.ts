35e00026bc5bd0f1351f9ba5c6b23b9abd7c51c5
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 13:08:52 +0100

    Step 3.1: Add mutations

--- types.d.ts
+++ types.d.ts
@@ -98,6 +98,38 @@ export interface Recipient {
   readAt?: Maybe<Date>;
 }
 
+export interface Mutation {
+  updateUser: User;
+
+  addChat?: Maybe<Chat>;
+
+  addGroup?: Maybe<Chat>;
+
+  updateGroup?: Maybe<Chat>;
+
+  removeChat?: Maybe<string>;
+
+  addMessage?: Maybe<Message>;
+
+  removeMessages?: Maybe<(Maybe<string>)[]>;
+
+  addMembers?: Maybe<(Maybe<string>)[]>;
+
+  removeMembers?: Maybe<(Maybe<string>)[]>;
+
+  addAdmins?: Maybe<(Maybe<string>)[]>;
+
+  removeAdmins?: Maybe<(Maybe<string>)[]>;
+
+  setGroupName?: Maybe<string>;
+
+  setGroupPicture?: Maybe<string>;
+
+  markAsReceived?: Maybe<boolean>;
+
+  markAsRead?: Maybe<boolean>;
+}
+
 // ====================================================
 // Arguments
 // ====================================================
@@ -107,6 +139,75 @@ export interface ChatQueryArgs {
 }
 export interface MessagesChatArgs {
   amount?: Maybe<number>;
+}
+export interface UpdateUserMutationArgs {
+  name?: Maybe<string>;
+
+  picture?: Maybe<string>;
+}
+export interface AddChatMutationArgs {
+  userId: string;
+}
+export interface AddGroupMutationArgs {
+  userIds: string[];
+
+  groupName: string;
+
+  groupPicture?: Maybe<string>;
+}
+export interface UpdateGroupMutationArgs {
+  chatId: string;
+
+  groupName?: Maybe<string>;
+
+  groupPicture?: Maybe<string>;
+}
+export interface RemoveChatMutationArgs {
+  chatId: string;
+}
+export interface AddMessageMutationArgs {
+  chatId: string;
+
+  content: string;
+}
+export interface RemoveMessagesMutationArgs {
+  chatId: string;
+
+  messageIds?: Maybe<(Maybe<string>)[]>;
+
+  all?: Maybe<boolean>;
+}
+export interface AddMembersMutationArgs {
+  groupId: string;
+
+  userIds: string[];
+}
+export interface RemoveMembersMutationArgs {
+  groupId: string;
+
+  userIds: string[];
+}
+export interface AddAdminsMutationArgs {
+  groupId: string;
+
+  userIds: string[];
+}
+export interface RemoveAdminsMutationArgs {
+  groupId: string;
+
+  userIds: string[];
+}
+export interface SetGroupNameMutationArgs {
+  groupId: string;
+}
+export interface SetGroupPictureMutationArgs {
+  groupId: string;
+}
+export interface MarkAsReceivedMutationArgs {
+  chatId: string;
+}
+export interface MarkAsReadMutationArgs {
+  chatId: string;
 }
 
 import {
@@ -454,6 +555,227 @@ export namespace RecipientResolvers {
   > = Resolver<R, Parent, TContext>;
 }
 
+export namespace MutationResolvers {
+  export interface Resolvers<TContext = {}, TypeParent = {}> {
+    updateUser?: UpdateUserResolver<UserDb, TypeParent, TContext>;
+
+    addChat?: AddChatResolver<Maybe<ChatDb>, TypeParent, TContext>;
+
+    addGroup?: AddGroupResolver<Maybe<ChatDb>, TypeParent, TContext>;
+
+    updateGroup?: UpdateGroupResolver<Maybe<ChatDb>, TypeParent, TContext>;
+
+    removeChat?: RemoveChatResolver<Maybe<string>, TypeParent, TContext>;
+
+    addMessage?: AddMessageResolver<Maybe<MessageDb>, TypeParent, TContext>;
+
+    removeMessages?: RemoveMessagesResolver<
+      Maybe<(Maybe<string>)[]>,
+      TypeParent,
+      TContext
+    >;
+
+    addMembers?: AddMembersResolver<
+      Maybe<(Maybe<string>)[]>,
+      TypeParent,
+      TContext
+    >;
+
+    removeMembers?: RemoveMembersResolver<
+      Maybe<(Maybe<string>)[]>,
+      TypeParent,
+      TContext
+    >;
+
+    addAdmins?: AddAdminsResolver<
+      Maybe<(Maybe<string>)[]>,
+      TypeParent,
+      TContext
+    >;
+
+    removeAdmins?: RemoveAdminsResolver<
+      Maybe<(Maybe<string>)[]>,
+      TypeParent,
+      TContext
+    >;
+
+    setGroupName?: SetGroupNameResolver<Maybe<string>, TypeParent, TContext>;
+
+    setGroupPicture?: SetGroupPictureResolver<
+      Maybe<string>,
+      TypeParent,
+      TContext
+    >;
+
+    markAsReceived?: MarkAsReceivedResolver<
+      Maybe<boolean>,
+      TypeParent,
+      TContext
+    >;
+
+    markAsRead?: MarkAsReadResolver<Maybe<boolean>, TypeParent, TContext>;
+  }
+
+  export type UpdateUserResolver<
+    R = UserDb,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, UpdateUserArgs>;
+  export interface UpdateUserArgs {
+    name?: Maybe<string>;
+
+    picture?: Maybe<string>;
+  }
+
+  export type AddChatResolver<
+    R = Maybe<ChatDb>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, AddChatArgs>;
+  export interface AddChatArgs {
+    userId: string;
+  }
+
+  export type AddGroupResolver<
+    R = Maybe<ChatDb>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, AddGroupArgs>;
+  export interface AddGroupArgs {
+    userIds: string[];
+
+    groupName: string;
+
+    groupPicture?: Maybe<string>;
+  }
+
+  export type UpdateGroupResolver<
+    R = Maybe<ChatDb>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, UpdateGroupArgs>;
+  export interface UpdateGroupArgs {
+    chatId: string;
+
+    groupName?: Maybe<string>;
+
+    groupPicture?: Maybe<string>;
+  }
+
+  export type RemoveChatResolver<
+    R = Maybe<string>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, RemoveChatArgs>;
+  export interface RemoveChatArgs {
+    chatId: string;
+  }
+
+  export type AddMessageResolver<
+    R = Maybe<MessageDb>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, AddMessageArgs>;
+  export interface AddMessageArgs {
+    chatId: string;
+
+    content: string;
+  }
+
+  export type RemoveMessagesResolver<
+    R = Maybe<(Maybe<string>)[]>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, RemoveMessagesArgs>;
+  export interface RemoveMessagesArgs {
+    chatId: string;
+
+    messageIds?: Maybe<(Maybe<string>)[]>;
+
+    all?: Maybe<boolean>;
+  }
+
+  export type AddMembersResolver<
+    R = Maybe<(Maybe<string>)[]>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, AddMembersArgs>;
+  export interface AddMembersArgs {
+    groupId: string;
+
+    userIds: string[];
+  }
+
+  export type RemoveMembersResolver<
+    R = Maybe<(Maybe<string>)[]>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, RemoveMembersArgs>;
+  export interface RemoveMembersArgs {
+    groupId: string;
+
+    userIds: string[];
+  }
+
+  export type AddAdminsResolver<
+    R = Maybe<(Maybe<string>)[]>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, AddAdminsArgs>;
+  export interface AddAdminsArgs {
+    groupId: string;
+
+    userIds: string[];
+  }
+
+  export type RemoveAdminsResolver<
+    R = Maybe<(Maybe<string>)[]>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, RemoveAdminsArgs>;
+  export interface RemoveAdminsArgs {
+    groupId: string;
+
+    userIds: string[];
+  }
+
+  export type SetGroupNameResolver<
+    R = Maybe<string>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, SetGroupNameArgs>;
+  export interface SetGroupNameArgs {
+    groupId: string;
+  }
+
+  export type SetGroupPictureResolver<
+    R = Maybe<string>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, SetGroupPictureArgs>;
+  export interface SetGroupPictureArgs {
+    groupId: string;
+  }
+
+  export type MarkAsReceivedResolver<
+    R = Maybe<boolean>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, MarkAsReceivedArgs>;
+  export interface MarkAsReceivedArgs {
+    chatId: string;
+  }
+
+  export type MarkAsReadResolver<
+    R = Maybe<boolean>,
+    Parent = {},
+    TContext = {}
+  > = Resolver<R, Parent, TContext, MarkAsReadArgs>;
+  export interface MarkAsReadArgs {
+    chatId: string;
+  }
+}
+
 /** Directs the executor to skip this field or fragment when the `if` argument is true. */
 export type SkipDirectiveResolver<Result> = DirectiveResolverFn<
   Result,
@@ -497,6 +819,7 @@ export interface IResolvers<TContext = {}> {
   Chat?: ChatResolvers.Resolvers<TContext>;
   Message?: MessageResolvers.Resolvers<TContext>;
   Recipient?: RecipientResolvers.Resolvers<TContext>;
+  Mutation?: MutationResolvers.Resolvers<TContext>;
   Date?: GraphQLScalarType;
 }
 
