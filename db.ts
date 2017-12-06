93ca59097cf5a31727013c082d5b5a749dac698c
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 12:51:58 +0100

    Step 1.3: Add resolvers and schema

--- db.ts
+++ db.ts
@@ -1,11 +1,5 @@
 import * as moment from 'moment';
 
-export const random = false;
-
-export function getRandomId() {
-  return Math.round(Math.random() * 1000000000000);
-}
-
 export enum MessageType {
   PICTURE,
   TEXT,
@@ -26,11 +20,11 @@ export interface Chat {
   name?: string | null,
   picture?: string | null,
   // All members, current and past ones.
-  userIds: number[],
+  allTimeMemberIds: number[],
   // Whoever gets the chat listed. For groups includes past members who still didn't delete the group.
-  listingIds: number[],
+  listingMemberIds: number[],
   // Actual members of the group (they are not the only ones who get the group listed). Null for chats.
-  memberIds?: number[] | null,
+  actualGroupMemberIds?: number[] | null,
   adminIds?: number[] | null,
   ownerId?: number | null,
   messages: Message[],
@@ -38,6 +32,7 @@ export interface Chat {
 
 export interface Message {
   id: number,
+  chatId: number,
   senderId: number,
   content: string,
   createdAt: number,
@@ -47,7 +42,9 @@ export interface Message {
 }
 
 export interface Recipient {
-  id: number,
+  userId: number,
+  messageId: number,
+  chatId: number,
   receivedAt: number | null,
   readAt: number | null,
 }
@@ -116,20 +113,23 @@ const chats: Chat[] = [
     id: 1,
     name: null,
     picture: null,
-    userIds: [1, 3],
-    listingIds: [1, 3],
+    allTimeMemberIds: [1, 3],
+    listingMemberIds: [1, 3],
     adminIds: null,
     ownerId: null,
     messages: [
       {
-        id: random? getRandomId() : 1,
+        id: 1,
+        chatId: 1,
         senderId: 1,
         content: 'You on your way?',
         createdAt: moment().subtract(1, 'hours').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 3,
+            userId: 3,
+            messageId: 1,
+            chatId: 1,
             receivedAt: null,
             readAt: null,
           },
@@ -137,14 +137,17 @@ const chats: Chat[] = [
         holderIds: [1, 3],
       },
       {
-        id: random? getRandomId() : 2,
+        id: 2,
+        chatId: 1,
         senderId: 3,
         content: 'Yep!',
         createdAt: moment().subtract(1, 'hours').add(5, 'minutes').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 1,
+            userId: 1,
+            messageId: 2,
+            chatId: 1,
             receivedAt: null,
             readAt: null,
           },
@@ -157,20 +160,23 @@ const chats: Chat[] = [
     id: 2,
     name: null,
     picture: null,
-    userIds: [1, 4],
-    listingIds: [1, 4],
+    allTimeMemberIds: [1, 4],
+    listingMemberIds: [1, 4],
     adminIds: null,
     ownerId: null,
     messages: [
       {
-        id: random? getRandomId() : 3,
+        id: 3,
+        chatId: 2,
         senderId: 1,
         content: 'Hey, it\'s me',
         createdAt: moment().subtract(2, 'hours').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 4,
+            userId: 4,
+            messageId: 3,
+            chatId: 2,
             receivedAt: null,
             readAt: null,
           },
@@ -183,20 +189,23 @@ const chats: Chat[] = [
     id: 3,
     name: null,
     picture: null,
-    userIds: [1, 5],
-    listingIds: [1, 5],
+    allTimeMemberIds: [1, 5],
+    listingMemberIds: [1, 5],
     adminIds: null,
     ownerId: null,
     messages: [
       {
-        id: random? getRandomId() : 4,
+        id: 4,
+        chatId: 3,
         senderId: 1,
         content: 'I should buy a boat',
         createdAt: moment().subtract(1, 'days').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 5,
+            userId: 5,
+            messageId: 4,
+            chatId: 3,
             receivedAt: null,
             readAt: null,
           },
@@ -204,14 +213,17 @@ const chats: Chat[] = [
         holderIds: [1, 5],
       },
       {
-        id: random? getRandomId() : 5,
+        id: 5,
+        chatId: 3,
         senderId: 1,
         content: 'You still there?',
         createdAt: moment().subtract(1, 'days').add(16, 'hours').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 5,
+            userId: 5,
+            messageId: 5,
+            chatId: 3,
             receivedAt: null,
             readAt: null,
           },
@@ -224,20 +236,23 @@ const chats: Chat[] = [
     id: 4,
     name: null,
     picture: null,
-    userIds: [3, 4],
-    listingIds: [3, 4],
+    allTimeMemberIds: [3, 4],
+    listingMemberIds: [3, 4],
     adminIds: null,
     ownerId: null,
     messages: [
       {
-        id: random? getRandomId() : 6,
+        id: 6,
+        chatId: 4,
         senderId: 3,
         content: 'Look at my mukluks!',
         createdAt: moment().subtract(4, 'days').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 4,
+            userId: 4,
+            messageId: 6,
+            chatId: 4,
             receivedAt: null,
             readAt: null,
           },
@@ -250,20 +265,23 @@ const chats: Chat[] = [
     id: 5,
     name: null,
     picture: null,
-    userIds: [2, 5],
-    listingIds: [2, 5],
+    allTimeMemberIds: [2, 5],
+    listingMemberIds: [2, 5],
     adminIds: null,
     ownerId: null,
     messages: [
       {
-        id: random? getRandomId() : 7,
+        id: 7,
+        chatId: 5,
         senderId: 2,
         content: 'This is wicked good ice cream.',
         createdAt: moment().subtract(2, 'weeks').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 5,
+            userId: 5,
+            messageId: 7,
+            chatId: 5,
             receivedAt: null,
             readAt: null,
           },
@@ -271,14 +289,17 @@ const chats: Chat[] = [
         holderIds: [2, 5],
       },
       {
-        id: random? getRandomId() : 8,
+        id: 8,
+        chatId: 6,
         senderId: 5,
         content: 'Love it!',
         createdAt: moment().subtract(2, 'weeks').add(10, 'minutes').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 2,
+            userId: 2,
+            messageId: 8,
+            chatId: 5,
             receivedAt: null,
             readAt: null,
           },
@@ -291,8 +312,8 @@ const chats: Chat[] = [
     id: 6,
     name: null,
     picture: null,
-    userIds: [1, 6],
-    listingIds: [1],
+    allTimeMemberIds: [1, 6],
+    listingMemberIds: [1],
     adminIds: null,
     ownerId: null,
     messages: [],
@@ -301,8 +322,8 @@ const chats: Chat[] = [
     id: 7,
     name: null,
     picture: null,
-    userIds: [2, 1],
-    listingIds: [2],
+    allTimeMemberIds: [2, 1],
+    listingMemberIds: [2],
     adminIds: null,
     ownerId: null,
     messages: [],
@@ -311,31 +332,38 @@ const chats: Chat[] = [
     id: 8,
     name: 'A user 0 group',
     picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
-    userIds: [1, 3, 4, 6],
-    listingIds: [1, 3, 4, 6],
-    memberIds: [1, 4, 6],
+    allTimeMemberIds: [1, 3, 4, 6],
+    listingMemberIds: [1, 3, 4, 6],
+    actualGroupMemberIds: [1, 4, 6],
     adminIds: [1, 6],
     ownerId: 1,
     messages: [
       {
-        id: random? getRandomId() : 9,
+        id: 9,
+        chatId: 8,
         senderId: 1,
         content: 'I made a group',
         createdAt: moment().subtract(2, 'weeks').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 3,
+            userId: 3,
+            messageId: 9,
+            chatId: 8,
             receivedAt: null,
             readAt: null,
           },
           {
-            id: 4,
+            userId: 4,
+            messageId: 9,
+            chatId: 8,
             receivedAt: moment().subtract(2, 'weeks').add(1, 'minutes').unix(),
             readAt: moment().subtract(2, 'weeks').add(5, 'minutes').unix(),
           },
           {
-            id: 6,
+            userId: 6,
+            messageId: 9,
+            chatId: 8,
             receivedAt: null,
             readAt: null,
           },
@@ -343,19 +371,24 @@ const chats: Chat[] = [
         holderIds: [1, 3, 4, 6],
       },
       {
-        id: random? getRandomId() : 10,
+        id: 10,
+        chatId: 8,
         senderId: 1,
         content: 'Ops, user 3 was not supposed to be here',
         createdAt: moment().subtract(2, 'weeks').add(2, 'minutes').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 4,
+            userId: 4,
+            messageId: 10,
+            chatId: 8,
             receivedAt: moment().subtract(2, 'weeks').add(3, 'minutes').unix(),
             readAt: moment().subtract(2, 'weeks').add(5, 'minutes').unix(),
           },
           {
-            id: 6,
+            userId: 6,
+            messageId: 10,
+            chatId: 8,
             receivedAt: null,
             readAt: null,
           },
@@ -363,19 +396,24 @@ const chats: Chat[] = [
         holderIds: [1, 4, 6],
       },
       {
-        id: random? getRandomId() : 11,
+        id: 11,
+        chatId: 8,
         senderId: 4,
         content: 'Awesome!',
         createdAt: moment().subtract(2, 'weeks').add(10, 'minutes').unix(),
         type: MessageType.TEXT,
         recipients: [
           {
-            id: 1,
+            userId: 1,
+            messageId: 11,
+            chatId: 8,
             receivedAt: null,
             readAt: null,
           },
           {
-            id: 6,
+            userId: 6,
+            messageId: 11,
+            chatId: 8,
             receivedAt: null,
             readAt: null,
           },
@@ -388,9 +426,9 @@ const chats: Chat[] = [
     id: 9,
     name: 'A user 5 group',
     picture: null,
-    userIds: [6, 3],
-    listingIds: [6, 3],
-    memberIds: [6, 3],
+    allTimeMemberIds: [6, 3],
+    listingMemberIds: [6, 3],
+    actualGroupMemberIds: [6, 3],
     adminIds: [6],
     ownerId: 6,
     messages: [],
