cc312b089ff4ff181b7eadbf75beb5f2fb2e740f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 13:01:46 +0100

    Step 2.3: Use our types

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,6 +1,6 @@
-import { IResolvers } from 'apollo-server-express';
+import { db } from "../db";
 import { GraphQLDateTime } from 'graphql-iso-date';
-import { ChatDb, db, MessageDb, RecipientDb, UserDb } from "../db";
+import { IResolvers } from '../types';
 
 let users = db.users;
 let chats = db.chats;
@@ -9,57 +9,57 @@ const currentUser = users[0];
 export const resolvers: IResolvers = {
   Date: GraphQLDateTime,
   Query: {
-    me: (): UserDb => currentUser,
-    users: (): UserDb[] => users.filter(user => user.id !== currentUser.id),
-    chats: (): ChatDb[] => chats.filter(chat => chat.listingMemberIds.includes(currentUser.id)),
-    chat: (obj: any, {chatId}): ChatDb | null => chats.find(chat => chat.id === chatId) || null,
+    me: () => currentUser,
+    users: () => users.filter(user => user.id !== currentUser.id),
+    chats: () => chats.filter(chat => chat.listingMemberIds.includes(currentUser.id)),
+    chat: (obj, {chatId}) => chats.find(chat => chat.id === Number(chatId)),
   },
   Chat: {
-    name: (chat: ChatDb): string => chat.name ? chat.name : users
+    name: (chat) => chat.name ? chat.name : users
       .find(user => user.id === chat.allTimeMemberIds.find(userId => userId !== currentUser.id))!.name,
-    picture: (chat: ChatDb) => chat.name ? chat.picture : users
+    picture: (chat) => chat.name ? chat.picture : users
       .find(user => user.id === chat.allTimeMemberIds.find(userId => userId !== currentUser.id))!.picture,
-    allTimeMembers: (chat: ChatDb): UserDb[] => users.filter(user => chat.allTimeMemberIds.includes(user.id)),
-    listingMembers: (chat: ChatDb): UserDb[] => users.filter(user => chat.listingMemberIds.includes(user.id)),
-    actualGroupMembers: (chat: ChatDb): UserDb[] => users.filter(user => chat.actualGroupMemberIds && chat.actualGroupMemberIds.includes(user.id)),
-    admins: (chat: ChatDb): UserDb[] => users.filter(user => chat.adminIds && chat.adminIds.includes(user.id)),
-    owner: (chat: ChatDb): UserDb | null => users.find(user => chat.ownerId === user.id) || null,
-    isGroup: (chat: ChatDb): boolean => !!chat.name,
-    messages: (chat: ChatDb, {amount = 0}: {amount: number}): MessageDb[] => {
+    allTimeMembers: (chat) => users.filter(user => chat.allTimeMemberIds.includes(user.id)),
+    listingMembers: (chat) => users.filter(user => chat.listingMemberIds.includes(user.id)),
+    actualGroupMembers: (chat) => users.filter(user => chat.actualGroupMemberIds && chat.actualGroupMemberIds.includes(user.id)),
+    admins: (chat) => users.filter(user => chat.adminIds && chat.adminIds.includes(user.id)),
+    owner: (chat) => users.find(user => chat.ownerId === user.id) || null,
+    isGroup: (chat) => !!chat.name,
+    messages: (chat, {amount = 0}) => {
       const messages = chat.messages
       .filter(message => message.holderIds.includes(currentUser.id))
-      .sort((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf()) || <MessageDb[]>[];
+      .sort((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf()) || [];
       return (amount ? messages.slice(0, amount) : messages).reverse();
     },
-    lastMessage: (chat: ChatDb): MessageDb => {
+    lastMessage: (chat) => {
       return chat.messages
         .filter(message => message.holderIds.includes(currentUser.id))
         .sort((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf())[0] || null;
     },
-    updatedAt: (chat: ChatDb): Date => {
+    updatedAt: (chat) => {
       const lastMessage = chat.messages
         .filter(message => message.holderIds.includes(currentUser.id))
         .sort((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf())[0];
 
       return lastMessage ? lastMessage.createdAt : chat.createdAt;
     },
-    unreadMessages: (chat: ChatDb): number => chat.messages
+    unreadMessages: (chat) => chat.messages
       .filter(message => message.holderIds.includes(currentUser.id) &&
         message.recipients.find(recipient => recipient.userId === currentUser.id && !recipient.readAt))
       .length,
   },
   Message: {
-    chat: (message: MessageDb): ChatDb | null => chats.find(chat => message.chatId === chat.id) || null,
-    sender: (message: MessageDb): UserDb | null => users.find(user => user.id === message.senderId) || null,
-    holders: (message: MessageDb): UserDb[] => users.filter(user => message.holderIds.includes(user.id)),
-    ownership: (message: MessageDb): boolean => message.senderId === currentUser.id,
+    chat: (message) => chats.find(chat => message.chatId === chat.id)!,
+    sender: (message) => users.find(user => user.id === message.senderId)!,
+    holders: (message) => users.filter(user => message.holderIds.includes(user.id)),
+    ownership: (message) => message.senderId === currentUser.id,
   },
   Recipient: {
-    user: (recipient: RecipientDb): UserDb | null => users.find(user => recipient.userId === user.id) || null,
-    message: (recipient: RecipientDb): MessageDb | null => {
-      const chat = chats.find(chat => recipient.chatId === chat.id);
-      return chat ? chat.messages.find(message => recipient.messageId === message.id) || null : null;
+    user: (recipient) => users.find(user => recipient.userId === user.id)!,
+    message: (recipient) => {
+      const chat = chats.find(chat => recipient.chatId === chat.id)!;
+      return chat.messages.find(message => recipient.messageId === message.id)!;
     },
-    chat: (recipient: RecipientDb): ChatDb | null => chats.find(chat => recipient.chatId === chat.id) || null,
+    chat: (recipient) => chats.find(chat => recipient.chatId === chat.id)!,
   },
 };
