ef2cb78accdecade7b3455837e8d66d2d53170d5
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-02-20 18:29:23 +0100

    Step 6.1: TypeORM with PostgreSQL

---
+++ entity/Recipient.ts
@@ -0,0 +1,40 @@
+import { Entity, ManyToOne, Column } from "typeorm";
+import { Message } from "./Message";
+import { User } from "./User";
+
+interface RecipientConstructor {
+  user?: User;
+  message?: Message;
+  receivedAt?: Date;
+  readAt?: Date;
+}
+
+@Entity()
+export class Recipient {
+  @ManyToOne(type => User, user => user.recipients, { primary: true })
+  user: User;
+
+  @ManyToOne(type => Message, message => message.recipients, { primary: true })
+  message: Message;
+
+  @Column({nullable: true})
+  receivedAt: Date;
+
+  @Column({nullable: true})
+  readAt: Date;
+
+  constructor({user, message, receivedAt, readAt}: RecipientConstructor = {}) {
+    if (user) {
+      this.user = user;
+    }
+    if (message) {
+      this.message = message;
+    }
+    if (receivedAt) {
+      this.receivedAt = receivedAt;
+    }
+    if (readAt) {
+      this.readAt = readAt;
+    }
+  }
+}
