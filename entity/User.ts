0aec0b7d0c9839daf707826d43b45a930d1ee02f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-02-20 18:29:23 +0100

    Step 6.1: TypeORM with PostgreSQL

---
+++ entity/User.ts
@@ -0,0 +1,75 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, OneToMany } from "typeorm";
+import { Chat } from "./Chat";
+import { Message } from "./Message";
+import { Recipient } from "./Recipient";
+
+interface UserConstructor {
+  username?: string;
+  password?: string;
+  name?: string;
+  picture?: string;
+  phone?: string;
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column({nullable: true})
+  picture: string;
+
+  @Column({nullable: true})
+  phone?: string;
+
+  @ManyToMany(type => Chat, chat => chat.allTimeMembers)
+  allTimeMemberChats: Chat[];
+
+  @ManyToMany(type => Chat, chat => chat.listingMembers)
+  listingMemberChats: Chat[];
+
+  @ManyToMany(type => Chat, chat => chat.actualGroupMembers)
+  actualGroupMemberChats: Chat[];
+
+  @ManyToMany(type => Chat, chat => chat.admins)
+  adminChats: Chat[];
+
+  @ManyToMany(type => Message, message => message.holders)
+  holderMessages: Message[];
+
+  @OneToMany(type => Chat, chat => chat.owner)
+  ownerChats: Chat[];
+
+  @OneToMany(type => Message, message => message.sender)
+  senderMessages: Message[];
+
+  @OneToMany(type => Recipient, recipient => recipient.user)
+  recipients: Recipient[];
+
+  constructor({username, password, name, picture, phone}: UserConstructor = {}) {
+    if (username) {
+      this.username = username;
+    }
+    if (password) {
+      this.password = password;
+    }
+    if (name) {
+      this.name = name;
+    }
+    if (picture) {
+      this.picture = picture;
+    }
+    if (phone) {
+      this.phone = phone;
+    }
+  }
+}
