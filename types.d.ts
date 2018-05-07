818cf2906069bcedb094574fc4fa4eee001365fc
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-05-07 18:16:09 +0200

    Step 7.1: Pagination

--- types.d.ts
+++ types.d.ts
@@ -6,6 +6,12 @@ export enum MessageType {
   Picture = "PICTURE"
 }
 
+export type Date = any;
+
+// ====================================================
+// Scalars
+// ====================================================
+
 // ====================================================
 // Types
 // ====================================================
@@ -47,6 +53,8 @@ export interface Chat {
 
   messages: (Maybe<Message>)[];
 
+  messageFeed?: Maybe<MessageFeed>;
+
   unreadMessages: number;
 
   isGroup: boolean;
@@ -61,7 +69,7 @@ export interface Message {
 
   content: string;
 
-  createdAt: string;
+  createdAt: Date;
 
   type: number;
 
@@ -79,9 +87,17 @@ export interface Recipient {
 
   chat: Chat;
 
-  receivedAt?: Maybe<string>;
+  receivedAt?: Maybe<Date>;
 
-  readAt?: Maybe<string>;
+  readAt?: Maybe<Date>;
+}
+
+export interface MessageFeed {
+  hasNextPage: boolean;
+
+  cursor?: Maybe<string>;
+
+  messages: (Maybe<Message>)[];
 }
 
 export interface Mutation {
@@ -127,6 +143,13 @@ export interface ChatQueryArgs {
 }
 export interface MessagesChatArgs {
   amount?: Maybe<number>;
+
+  before?: Maybe<string>;
+}
+export interface MessageFeedChatArgs {
+  amount?: Maybe<number>;
+
+  before?: Maybe<string>;
 }
 export interface AddChatMutationArgs {
   recipientId: string;
@@ -187,7 +210,11 @@ export interface MessageAddedSubscriptionArgs {
   chatId?: Maybe<string>;
 }
 
-import { GraphQLResolveInfo } from "graphql";
+import {
+  GraphQLResolveInfo,
+  GraphQLScalarType,
+  GraphQLScalarTypeConfig
+} from "graphql";
 
 export type Resolver<Result, Parent = {}, Context = {}, Args = {}> = (
   parent: Parent,
@@ -324,6 +351,8 @@ export namespace ChatResolvers {
 
     messages?: MessagesResolver<(Maybe<Message>)[], TypeParent, Context>;
 
+    messageFeed?: MessageFeedResolver<Maybe<MessageFeed>, TypeParent, Context>;
+
     unreadMessages?: UnreadMessagesResolver<number, TypeParent, Context>;
 
     isGroup?: IsGroupResolver<boolean, TypeParent, Context>;
@@ -376,6 +405,19 @@ export namespace ChatResolvers {
   > = Resolver<R, Parent, Context, MessagesArgs>;
   export interface MessagesArgs {
     amount?: Maybe<number>;
+
+    before?: Maybe<string>;
+  }
+
+  export type MessageFeedResolver<
+    R = Maybe<MessageFeed>,
+    Parent = Chat,
+    Context = {}
+  > = Resolver<R, Parent, Context, MessageFeedArgs>;
+  export interface MessageFeedArgs {
+    amount?: Maybe<number>;
+
+    before?: Maybe<string>;
   }
 
   export type UnreadMessagesResolver<
@@ -400,7 +442,7 @@ export namespace MessageResolvers {
 
     content?: ContentResolver<string, TypeParent, Context>;
 
-    createdAt?: CreatedAtResolver<string, TypeParent, Context>;
+    createdAt?: CreatedAtResolver<Date, TypeParent, Context>;
 
     type?: TypeResolver<number, TypeParent, Context>;
 
@@ -432,7 +474,7 @@ export namespace MessageResolvers {
     Context = {}
   > = Resolver<R, Parent, Context>;
   export type CreatedAtResolver<
-    R = string,
+    R = Date,
     Parent = Message,
     Context = {}
   > = Resolver<R, Parent, Context>;
@@ -466,9 +508,9 @@ export namespace RecipientResolvers {
 
     chat?: ChatResolver<Chat, TypeParent, Context>;
 
-    receivedAt?: ReceivedAtResolver<Maybe<string>, TypeParent, Context>;
+    receivedAt?: ReceivedAtResolver<Maybe<Date>, TypeParent, Context>;
 
-    readAt?: ReadAtResolver<Maybe<string>, TypeParent, Context>;
+    readAt?: ReadAtResolver<Maybe<Date>, TypeParent, Context>;
   }
 
   export type UserResolver<
@@ -487,13 +529,39 @@ export namespace RecipientResolvers {
     Context = {}
   > = Resolver<R, Parent, Context>;
   export type ReceivedAtResolver<
-    R = Maybe<string>,
+    R = Maybe<Date>,
     Parent = Recipient,
     Context = {}
   > = Resolver<R, Parent, Context>;
   export type ReadAtResolver<
-    R = Maybe<string>,
+    R = Maybe<Date>,
     Parent = Recipient,
+    Context = {}
+  > = Resolver<R, Parent, Context>;
+}
+
+export namespace MessageFeedResolvers {
+  export interface Resolvers<Context = {}, TypeParent = MessageFeed> {
+    hasNextPage?: HasNextPageResolver<boolean, TypeParent, Context>;
+
+    cursor?: CursorResolver<Maybe<string>, TypeParent, Context>;
+
+    messages?: MessagesResolver<(Maybe<Message>)[], TypeParent, Context>;
+  }
+
+  export type HasNextPageResolver<
+    R = boolean,
+    Parent = MessageFeed,
+    Context = {}
+  > = Resolver<R, Parent, Context>;
+  export type CursorResolver<
+    R = Maybe<string>,
+    Parent = MessageFeed,
+    Context = {}
+  > = Resolver<R, Parent, Context>;
+  export type MessagesResolver<
+    R = (Maybe<Message>)[],
+    Parent = MessageFeed,
     Context = {}
   > = Resolver<R, Parent, Context>;
 }
@@ -745,14 +813,20 @@ export interface DeprecatedDirectiveArgs {
   reason?: string;
 }
 
+export interface DateScalarConfig extends GraphQLScalarTypeConfig<Date, any> {
+  name: "Date";
+}
+
 export interface IResolvers {
   Query?: QueryResolvers.Resolvers;
   User?: UserResolvers.Resolvers;
   Chat?: ChatResolvers.Resolvers;
   Message?: MessageResolvers.Resolvers;
   Recipient?: RecipientResolvers.Resolvers;
+  MessageFeed?: MessageFeedResolvers.Resolvers;
   Mutation?: MutationResolvers.Resolvers;
   Subscription?: SubscriptionResolvers.Resolvers;
+  Date?: GraphQLScalarType;
 }
 
 export interface IDirectiveResolvers<Result> {
