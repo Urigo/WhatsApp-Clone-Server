e00894bea56846490592452700a336e657f8c703
Niccolò Belli <niccolo.belli@linuxsystems.it>
2019-01-26 17:28:31 +0100

    Step 7.11: Profile pictures upload

--- modules/user/providers/user.provider.ts
+++ modules/user/providers/user.provider.ts
@@ -1,14 +1,42 @@
-import { Injectable } from '@graphql-modules/di'
+import { Inject, Injectable } from '@graphql-modules/di'
 import { PubSub } from 'apollo-server-express'
 import { Connection } from 'typeorm'
 import { User } from "../../../entity/User";
+import { APP } from "../../app.symbols";
+import { Express } from "express";
+import multer from 'multer';
+import tmp from 'tmp';
+import '../../../types/cloudinary';
+import cloudinary from 'cloudinary';
+export const CLOUDINARY_URL = process.env.CLOUDINARY_URL || '';
 
 @Injectable()
 export class UserProvider {
   constructor(
     private pubsub: PubSub,
     private connection: Connection,
+    @Inject(APP) private app: Express,
   ) {
+    const match = CLOUDINARY_URL.match(/cloudinary:\/\/(\d+):(\w+)@(\.+)/);
+
+    if (match) {
+      const [api_key, api_secret, cloud_name] = match.slice(1);
+      cloudinary.config({ api_key, api_secret, cloud_name });
+    }
+
+    const upload = multer({
+      dest: tmp.dirSync({ unsafeCleanup: true }).name,
+    });
+
+    app.post('/upload-profile-pic', upload.single('file'), (req: any, res, done) => {
+      cloudinary.v2.uploader.upload(req.file.path, (error, result) => {
+        if (error) {
+          done(error);
+        } else {
+          res.json(result);
+        }
+      })
+    });
   }
 
   getMe(currentUser: User) {
