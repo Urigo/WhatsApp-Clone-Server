2aee353f3d7bcf8cfafa979be3c6fca4356421dd
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-01-26 19:11:11 -0500

    Refactor hook

--- modules/auth/providers/auth.provider.ts
+++ modules/auth/providers/auth.provider.ts
@@ -3,12 +3,12 @@ import { Connection } from 'typeorm';
 import { User } from '../../../entity/User';
 import bcrypt from 'bcrypt-nodejs';
 import { PubSub } from 'apollo-server-express'
-import { ModuleSessionInfo } from '@graphql-modules/core';
+import { ModuleSessionInfo, OnRequest, OnConnect } from '@graphql-modules/core';
 
 @Injectable({
   scope: ProviderScope.Session
 })
-export class AuthProvider {
+export class AuthProvider implements OnRequest, OnConnect {
 
   currentUser: User;
   userRepository = this.connection.getRepository(User);
@@ -29,15 +29,12 @@ export class AuthProvider {
       const buf = new Buffer(connectionParams.authToken.split(' ')[1], 'base64');
       // Read it back out as a string
       const [username, password]: string[] = buf.toString().split(':');
-      if (username && password) {
-        const user = await this.connection.getRepository(User).findOne({ where: { username } });
-
-        if (user && this.validPassword(password, user.password)) {
-          // Set context for the WebSocket
-          this.currentUser = user;
-        } else {
-          throw new Error('Wrong credentials!');
-        }
+      const user = await this.signIn(username, password);
+      if (user) {
+        // Set context for the WebSocket
+        this.currentUser = user;
+      } else {
+        throw new Error('Wrong credentials!');
       }
     } else {
       throw new Error('Missing auth token!');
