d9d06d68a6d2f06864c30f4f4a2993e7e16ed1e5
DAB0mB <emanor6@gmail.com>
2019-02-06 00:51:50 +0800

    Step 1.6: Implement resolvers against TypeORM

--- entity/message.ts
+++ entity/message.ts
@@ -1,20 +1,80 @@
-import Recipient from './recipient'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+  CreateDateColumn,
+} from 'typeorm'
+import Chat from './chat'
+import User from './user'
+import { MessageType } from '../db'
 
-export enum MessageType {
-  PICTURE,
-  TEXT,
-  LOCATION,
+interface MessageConstructor {
+  sender?: User
+  content?: string
+  createdAt?: Date
+  type?: MessageType
+  holders?: User[]
+  chat?: Chat
 }
 
-export interface Message {
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn()
   id: string
-  chatId: string
-  senderId: string
+
+  @ManyToOne(type => User, user => user.senderMessages, { eager: true })
+  sender: User
+
+  @Column()
   content: string
-  createdAt: number
-  type: MessageType
-  recipients: Recipient[]
-  holderIds: string[]
+
+  @CreateDateColumn({ nullable: true })
+  createdAt: Date
+
+  @Column()
+  type: number
+
+  @ManyToMany(type => User, user => user.holderMessages, {
+    cascade: ['insert', 'update'],
+    eager: true,
+  })
+  @JoinTable()
+  holders: User[]
+
+  @ManyToOne(type => Chat, chat => chat.messages)
+  chat: Chat
+
+  constructor({
+    sender,
+    content,
+    createdAt,
+    type,
+    holders,
+    chat,
+  }: MessageConstructor = {}) {
+    if (sender) {
+      this.sender = sender
+    }
+    if (content) {
+      this.content = content
+    }
+    if (createdAt) {
+      this.createdAt = createdAt
+    }
+    if (type) {
+      this.type = type
+    }
+    if (holders) {
+      this.holders = holders
+    }
+    if (chat) {
+      this.chat = chat
+    }
+  }
 }
 
 export default Message
