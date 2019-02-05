b18795250a9df575e2b9dd0f7fd45ef5a88f6109
DAB0mB <emanor6@gmail.com>
2019-02-06 00:51:50 +0800

    Step 1.6: Implement resolvers against TypeORM

--- entity/user.ts
+++ entity/user.ts
@@ -1,10 +1,60 @@
-export interface User {
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, OneToMany } from 'typeorm'
+import Chat from './chat'
+import Message from './message'
+
+interface UserConstructor {
+  username?: string
+  password?: string
+  name?: string
+  picture?: string
+}
+
+@Entity('app_user')
+export class User {
+  @PrimaryGeneratedColumn()
   id: string
+
+  @Column()
   username: string
+
+  @Column()
   password: string
+
+  @Column()
   name: string
-  picture?: string | null
-  phone?: string | null
+
+  @Column({ nullable: true })
+  picture: string
+
+  @ManyToMany(type => Chat, chat => chat.allTimeMembers)
+  allTimeMemberChats: Chat[]
+
+  @ManyToMany(type => Chat, chat => chat.listingMembers)
+  listingMemberChats: Chat[]
+
+  @ManyToMany(type => Message, message => message.holders)
+  holderMessages: Message[]
+
+  @OneToMany(type => Chat, chat => chat.owner)
+  ownerChats: Chat[]
+
+  @OneToMany(type => Message, message => message.sender)
+  senderMessages: Message[]
+
+  constructor({ username, password, name, picture }: UserConstructor = {}) {
+    if (username) {
+      this.username = username
+    }
+    if (password) {
+      this.password = password
+    }
+    if (name) {
+      this.name = name
+    }
+    if (picture) {
+      this.picture = picture
+    }
+  }
 }
 
 export default User
--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,83 +1,197 @@
 import { IResolvers as IApolloResolvers } from 'apollo-server-express'
 import { GraphQLDateTime } from 'graphql-iso-date'
-import db from '../db'
 import Chat from '../entity/chat'
 import Message from '../entity/message'
-import Recipient from '../entity/recipient'
 import User from '../entity/user'
 import { IResolvers } from '../types'
-
-let users = db.users
-let chats = db.chats
-const currentUser: string = '1'
 
 export default {
   Date: GraphQLDateTime,
   Query: {
     // Show all users for the moment.
-    users: () => users.filter(user => user.id !== currentUser),
-    chats: () => chats.filter(chat => chat.listingMemberIds.includes(currentUser)),
-    chat: (obj, { chatId }) => chats.find(chat => chat.id === chatId) || null,
+    users: (root, args, { connection, currentUser }) => {
+      return connection.createQueryBuilder(User, 'user').where('user.id != :id', {id: currentUser.id}).getMany();
+    },
+
+    chats: (root, args, { connection, currentUser }) => {
+      return connection
+        .createQueryBuilder(Chat, 'chat')
+        .leftJoin('chat.listingMembers', 'listingMembers')
+        .where('listingMembers.id = :id', { id: currentUser.id })
+        .orderBy('chat.createdAt', 'DESC')
+        .getMany();
+    },
+
+    chat: async (root, { chatId }, { connection }) => {
+      const chat = await connection
+        .createQueryBuilder(Chat, 'chat')
+        .whereInIds(chatId)
+        .getOne();
+
+      return chat || null;
+    },
   },
+
   Chat: {
-    name: (chat) =>
-      chat.name
-        ? chat.name
-        : users.find(
-            user => user.id === chat.allTimeMemberIds.find((userId: string) => userId !== currentUser)
-          )!.name,
-    picture: (chat) =>
-      chat.name
-        ? chat.picture
-        : users.find(
-            user => user.id === chat.allTimeMemberIds.find((userId: string) => userId !== currentUser)
-          )!.picture,
-    allTimeMembers: (chat) =>
-      users.filter(user => chat.allTimeMemberIds.includes(user.id)),
-    listingMembers: (chat) =>
-      users.filter(user => chat.listingMemberIds.includes(user.id)),
-    actualGroupMembers: (chat) =>
-      users.filter(
-        user => chat.actualGroupMemberIds && chat.actualGroupMemberIds.includes(user.id)
-      ),
-    admins: (chat) =>
-      users.filter(user => chat.adminIds && chat.adminIds.includes(user.id)),
-    owner: (chat) => users.find(user => chat.ownerId === user.id) || null,
-    messages: (chat, { amount = 0 }) => {
-      const messages =
-        chat.messages
-          .filter((message: Message) => message.holderIds.includes(currentUser))
-          .sort((a: Message, b: Message) => b.createdAt - a.createdAt) || []
-      return (amount ? messages.slice(0, amount) : messages).reverse()
+    name: async (chat, args, { connection, currentUser }) => {
+      if (chat.name) {
+        return chat.name;
+      }
+
+      const user = await connection
+        .createQueryBuilder(User, 'user')
+        .where('user.id != :userId', { userId: currentUser.id })
+        .innerJoin(
+          'user.allTimeMemberChats',
+          'allTimeMemberChats',
+          'allTimeMemberChats.id = :chatId',
+          { chatId: chat.id },
+        )
+        .getOne();
+
+      return user ? user.name : null
     },
-    unreadMessages: (chat) =>
-      chat.messages.filter(
-        (message: Message) =>
-          message.holderIds.includes(currentUser) &&
-          message.recipients.find(
-            (recipient: Recipient) => recipient.userId === currentUser && !recipient.readAt
-          )
-      ).length,
-    lastMessage: (chat) => chat.messages[chat.messages.length - 1],
-    isGroup: (chat) => !!chat.name,
+
+    picture: async (chat, args, { connection, currentUser }) => {
+      if (chat.picture) {
+        return chat.picture;
+      }
+
+      const user = await connection
+        .createQueryBuilder(User, 'user')
+        .where('user.id != :userId', { userId: currentUser.id })
+        .innerJoin(
+          'user.allTimeMemberChats',
+          'allTimeMemberChats',
+          'allTimeMemberChats.id = :chatId',
+          { chatId: chat.id },
+        )
+        .getOne();
+
+      return user ? user.picture : null
+    },
+
+    allTimeMembers: (chat, args, { connection }) => {
+      return connection
+        .createQueryBuilder(User, 'user')
+        .innerJoin(
+          'user.listingMemberChats',
+          'listingMemberChats',
+          'listingMemberChats.id = :chatId',
+          { chatId: chat.id },
+        )
+        .getMany()
+    },
+
+    listingMembers: (chat, args, { connection }) => {
+      return connection
+        .createQueryBuilder(User, 'user')
+        .innerJoin(
+          'user.listingMemberChats',
+          'listingMemberChats',
+          'listingMemberChats.id = :chatId',
+          { chatId: chat.id },
+        )
+        .getMany();
+    },
+
+    owner: async (chat, args, { connection }) => {
+      const owner = await connection
+        .createQueryBuilder(User, 'user')
+        .innerJoin('user.ownerChats', 'ownerChats', 'ownerChats.id = :chatId', {
+          chatId: chat.id,
+        })
+        .getOne();
+
+      return owner || null;
+    },
+
+    messages: async (chat, { amount = 0 }, { connection, currentUser }) => {
+      if (chat.messages) {
+        return amount ? chat.messages.slice(-amount) : chat.messages;
+      }
+
+      let query = connection
+        .createQueryBuilder(Message, 'message')
+        .innerJoin('message.chat', 'chat', 'chat.id = :chatId', { chatId: chat.id })
+        .innerJoin('message.holders', 'holders', 'holders.id = :userId', {
+          userId: currentUser.id,
+        })
+        .orderBy({ 'message.createdAt': { order: 'DESC', nulls: 'NULLS LAST' } });
+
+      if (amount) {
+        query = query.take(amount);
+      }
+
+      return (await query.getMany()).reverse();
+    },
+
+    lastMessage: async (chat, args, { connection, currentUser }) => {
+      if (chat.messages) {
+        return chat.messages.length ? chat.messages[chat.messages.length - 1] : null;
+      }
+
+      const messages = await connection
+        .createQueryBuilder(Message, 'message')
+        .innerJoin('message.chat', 'chat', 'chat.id = :chatId', { chatId: chat.id })
+        .innerJoin('message.holders', 'holders', 'holders.id = :userId', {
+          userId: currentUser.id,
+        })
+        .orderBy({ 'message.createdAt': { order: 'DESC', nulls: 'NULLS LAST' } })
+        .getMany()
+
+      return messages && messages.length ? messages[messages.length - 1] : null;
+    },
   },
+
   Message: {
-    chat: (message) =>
-      chats.find(chat => message.chatId === chat.id) || null,
-    sender: (message) =>
-      users.find(user => user.id === message.senderId) || null,
-    holders: (message) =>
-      users.filter(user => message.holderIds.includes(user.id)),
-    ownership: (message) => message.senderId === currentUser,
-  },
-  Recipient: {
-    user: (recipient) =>
-      users.find(user => recipient.userId === user.id) || null,
-    message: (recipient) => {
-      const chat = chats.find(chat => recipient.chatId === chat.id)
-      return chat ? chat.messages.find(message => recipient.messageId === message.id) || null : null
+    chat: async (message, args, { connection }) => {
+      const chat = await connection
+        .createQueryBuilder(Chat, 'chat')
+        .innerJoin('chat.messages', 'messages', 'messages.id = :messageId', {
+          messageId: message.id
+        })
+        .getOne();
+
+      if (!chat) {
+        throw new Error(`Message must have a chat.`);
+      }
+
+      return chat;
     },
-    chat: (recipient) =>
-      chats.find(chat => recipient.chatId === chat.id) || null,
+
+    sender: async (message, args, { connection }) => {
+      const sender = await connection
+        .createQueryBuilder(User, 'user')
+        .innerJoin('user.senderMessages', 'senderMessages', 'senderMessages.id = :messageId', {
+          messageId: message.id,
+        })
+        .getOne();
+
+      if (!sender) {
+        throw new Error(`Message must have a sender.`);
+      }
+
+      return sender;
+    },
+
+    holders: async (message, args, { connection }) => {
+      return connection
+        .createQueryBuilder(User, 'user')
+        .innerJoin('user.holderMessages', 'holderMessages', 'holderMessages.id = :messageId', {
+          messageId: message.id,
+        })
+        .getMany();
+    },
+
+    ownership: async (message, args, { connection, currentUser }) => {
+      return !!(await connection
+        .createQueryBuilder(User, 'user')
+        .whereInIds(currentUser.id)
+        .innerJoin('user.senderMessages', 'senderMessages', 'senderMessages.id = :messageId', {
+          messageId: message.id,
+        })
+        .getCount())
+    }
   },
 } as IResolvers as IApolloResolvers
