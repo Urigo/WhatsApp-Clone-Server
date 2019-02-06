033a6cd9aca537178a8475c027e8ee8dfe83cdbf
DAB0mB <emanor6@gmail.com>
2019-02-07 01:19:59 +0800

    Step 2.1: Add auth routes

--- modules/user/resolvers/resolvers.ts
+++ modules/user/resolvers/resolvers.ts
@@ -5,6 +5,7 @@ import { UserProvider } from '../providers/user.provider'
 
 export default {
   Query: {
+    me: (obj, args, { injector }) => injector.get(UserProvider).getMe(),
     users: (obj, args, { injector }) => injector.get(UserProvider).getUsers(),
   },
 } as IResolvers
