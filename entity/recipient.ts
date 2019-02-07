9192ee6e2d28b007322638978026a08fee3b69a1
DAB0mB <emanor6@gmail.com>
2019-02-08 00:51:08 +0800

    Step 3.1: Edit entities to serve chat app

---
+++ entity/recipient.ts
@@ -0,0 +1,42 @@
+import { Entity, ManyToOne, Column } from 'typeorm'
+import { Message } from './message'
+import { User } from './user'
+
+interface RecipientConstructor {
+  user?: User
+  message?: Message
+  receivedAt?: Date
+  readAt?: Date
+}
+
+@Entity()
+export class Recipient {
+  @ManyToOne(type => User, user => user.recipients, { primary: true })
+  user: User
+
+  @ManyToOne(type => Message, message => message.recipients, { primary: true })
+  message: Message
+
+  @Column({ nullable: true })
+  receivedAt: Date
+
+  @Column({ nullable: true })
+  readAt: Date
+
+  constructor({ user, message, receivedAt, readAt }: RecipientConstructor = {}) {
+    if (user) {
+      this.user = user
+    }
+    if (message) {
+      this.message = message
+    }
+    if (receivedAt) {
+      this.receivedAt = receivedAt
+    }
+    if (readAt) {
+      this.readAt = readAt
+    }
+  }
+}
+
+export default Recipient
