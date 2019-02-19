c2a4bcb148a6c0559aeefed0be35666028dadc09
Niccolò Belli <niccolo.belli@linuxsystems.it>
2019-02-19 11:23:10 +0100

    Step 8.2: Auth Module

--- schema/index.ts
+++
@@ -1,8 +0,0 @@
-import { makeExecutableSchema } from 'apollo-server-express';
-import typeDefs from './typeDefs';
-import { resolvers } from './resolvers';
-
-export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers: resolvers as any,
-});
--- schema/types.ts
+++
@@ -1,7 +0,0 @@
-import { Connection } from "typeorm";
-import { User } from "../entity/User";
-
-export interface AppContext {
-  currentUser: User;
-  connection: Connection;
-}
