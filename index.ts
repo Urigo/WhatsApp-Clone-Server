8969d87c2a339789fc51d7544519cb25b05da5da
DAB0mB <emanor6@gmail.com>
2019-03-10 22:05:21 +0800

    Step 1.3: Setup a Express with a basic health check route

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('hello world')
+import express from 'express'
+
+const app = express()
+
+app.get('/_ping', (req, res) => {
+  res.send('pong')
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
