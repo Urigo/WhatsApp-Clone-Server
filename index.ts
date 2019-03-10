074502698805f29fc624b1fcf84ccaa4b8c4bb70
DAB0mB <emanor6@gmail.com>
2019-03-10 22:09:12 +0800

    Step 1.5: Create GET /chats route

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { chats } from './db';
 
 const app = express();
 
 app.get('/_ping', (req, res) => {
   res.send('pong');
+});
+
+app.get('/chats', (req, res) => {
+  res.json(chats);
 });
 
 const port = process.env.PORT || 4000;
