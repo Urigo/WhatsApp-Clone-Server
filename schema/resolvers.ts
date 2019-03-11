f1e72951f44880fb945f8e02f59a6926f0c7384f
DAB0mB <emanor6@gmail.com>
2019-03-11 15:26:48 +0800

    Step 2.3: Resolve Chat.lastMessage

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,9 +1,15 @@
 import { DateTimeResolver, URLResolver } from 'graphql-scalars';
-import { chats } from '../db';
+import { chats, messages } from '../db';
 
 const resolvers = {
   Date: DateTimeResolver,
   URL: URLResolver,
+
+  Chat: {
+    lastMessage(chat: any) {
+      return messages.find(m => m.id === chat.lastMessage);
+    },
+  },
 
   Query: {
     chats() {
