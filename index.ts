81fdf30467d6fd3a944a611c3b5bdde5ec35cf52
DAB0mB <emanor6@gmail.com>
2019-03-11 13:01:22 +0800

    Step 2.1: Setup Apollo GraphQL

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
+import { ApolloServer, gql } from 'apollo-server-express';
 import cors from 'cors';
 import express from 'express';
 import { chats } from './db';
+import schema from './schema';
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/_ping', (req, res) => {
   res.send('pong');
@@ -12,6 +15,13 @@ app.get('/_ping', (req, res) => {
 
 app.get('/chats', (req, res) => {
   res.json(chats);
+});
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({
+  app,
+  path: '/graphql',
 });
 
 const port = process.env.PORT || 4000;
