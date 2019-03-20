e9580a973b41c1085b91c84a4ad0eadf35121b84
DAB0mB <emanor6@gmail.com>
2019-03-20 14:18:29 +0800

    Step 4.2: Resolve last message based on messages array

--- db.ts
+++ db.ts
@@ -28,28 +28,24 @@ export const chats = [
     id: '1',
     name: 'Ethan Gonzalez',
     picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-    lastMessage: '1',
     messages: ['1'],
   },
   {
     id: '2',
     name: 'Bryan Wallace',
     picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-    lastMessage: '2',
     messages: ['2'],
   },
   {
     id: '3',
     name: 'Avery Stewart',
     picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-    lastMessage: '3',
     messages: ['3'],
   },
   {
     id: '4',
     name: 'Katie Peterson',
     picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-    lastMessage: '4',
     messages: ['4'],
   },
 ];
