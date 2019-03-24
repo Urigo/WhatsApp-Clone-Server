d304ff9b430bd8474bbbf6cc123b9ec2c46b93a3
DAB0mB <emanor6@gmail.com>
2019-03-24 15:34:30 +0800

    Step 5.1: Add addMessage() mutation

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -26,6 +26,32 @@ const resolvers = {
       return chats.find(c => c.id === chatId);
     },
   },
+
+  Mutation: {
+    addMessage(root: any, { chatId, content }: any) {
+      const chatIndex = chats.findIndex(c => c.id === chatId);
+
+      if (chatIndex === -1) return null;
+
+      const chat = chats[chatIndex];
+      
+      const messagesIds = messages.map(currentMessage => Number(currentMessage.id));
+      const messageId = String(Math.max(...messagesIds) + 1);
+      const message = {
+        id: messageId,
+        createdAt: new Date(),
+        content,
+      };
+
+      messages.push(message);
+      chat.messages.push(messageId);
+      // The chat will appear at the top of the ChatsList component
+      chats.splice(chatIndex, 1);
+      chats.unshift(chat);
+
+      return message;
+    },
+  },
 };
 
 export default resolvers;
