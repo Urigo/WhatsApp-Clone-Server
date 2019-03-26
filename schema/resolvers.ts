86651569be596b56850172a0b6755e97412c93b9
DAB0mB <emanor6@gmail.com>
2019-03-27 00:55:16 +0800

    Step 7.5: Add Subscription.messageAdded resolver

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -57,6 +57,13 @@ const resolvers: Resolvers = {
       return message;
     },
   },
+
+  Subscription: {
+    messageAdded: {
+      subscribe: (root, args, { pubsub }) =>
+        pubsub.asyncIterator('messageAdded'),
+    },
+  },
 };
 
 export default resolvers;
