d7d0a1df4e436f6c456533b0e494d5adaa7d33d2
DAB0mB <emanor6@gmail.com>
2019-03-29 07:00:31 +0800

    Step 8.2: Resolve queries in relation to current user

--- tests/mutations/addMessage.test.ts
+++ tests/mutations/addMessage.test.ts
@@ -1,7 +1,7 @@
 import { createTestClient } from 'apollo-server-testing';
 import { ApolloServer, PubSub, gql } from 'apollo-server-express';
 import schema from '../../schema';
-import { resetDb } from '../../db';
+import { resetDb, users } from '../../db';
 
 describe('Mutation.addMessage', () => {
   beforeEach(resetDb);
@@ -9,7 +9,10 @@ describe('Mutation.addMessage', () => {
   it('should add message to specified chat', async () => {
     const server = new ApolloServer({
       schema,
-      context: () => ({ pubsub: new PubSub() }),
+      context: () => ({
+        pubsub: new PubSub(),
+        currentUser: users[0],
+      }),
     });
 
     const { query, mutate } = createTestClient(server);
