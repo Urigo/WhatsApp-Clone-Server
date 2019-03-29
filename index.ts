2cd39646e03464db41de272b8871a845910c6fb9
DAB0mB <emanor6@gmail.com>
2019-03-29 11:32:37 +0800

    Step 8.5: Use cookie parser

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer, gql, PubSub } from 'apollo-server-express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import express from 'express'
 import http from 'http'
 import { users } from './db'
@@ -11,6 +12,7 @@ const app = express()
 const origin = process.env.ORIGIN || 'http://localhost:3000'
 app.use(cors({ credentials: true, origin }))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 app.get('/_ping', (req, res) => {
   res.send('pong')
