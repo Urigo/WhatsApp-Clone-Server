5bd117baf5b2256a969dd8aa75156261ab22ce2b
DAB0mB <emanor6@gmail.com>
2019-04-01 13:22:19 +0800

    Step 8.4: Support credentials

--- index.ts
+++ index.ts
@@ -8,7 +8,8 @@ import schema from './schema';
 
 const app = express();
 
-app.use(cors());
+const origin = process.env.ORIGIN || 'http://localhost:3000';
+app.use(cors({ credentials: true, origin }));
 app.use(bodyParser.json());
 
 app.get('/_ping', (req, res) => {
@@ -27,6 +28,7 @@ const server = new ApolloServer({
 server.applyMiddleware({
   app,
   path: '/graphql',
+  cors: { credentials: true, origin },
 });
 
 const httpServer = http.createServer(app);
