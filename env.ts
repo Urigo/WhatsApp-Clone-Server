be03d3ae7d351871c17522160b2acb55d2e13002
DAB0mB <emanor6@gmail.com>
2019-04-07 16:51:34 +0800

    Step 10.2: Add signIn mutation

--- env.ts
+++ env.ts
@@ -1,2 +1,6 @@
+export const expiration = process.env.JWT_EXPIRATION_MS
+  ? parseInt(process.env.JWT_EXPIRATION_MS)
+  : 24 * 60 * 60 * 1000;
+export const secret = process.env.JWT_SECRET || '70p53cr37';
 export const origin = process.env.ORIGIN || 'http://localhost:3000';
 export const port = process.env.PORT || 4000;
