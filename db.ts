7bb484b32eb7b0d17f3cf20f080c77f4c30b200b
DAB0mB <emanor6@gmail.com>
2019-04-07 16:59:43 +0800

    Step 10.4: Update user schema to contain credentials

--- db.ts
+++ db.ts
@@ -1,6 +1,8 @@
 export type User = {
   id: string;
   name: string;
+  username: string;
+  password: string;
   picture: string;
 };
 
@@ -30,26 +32,41 @@ export const resetDb = () => {
       {
         id: '1',
         name: 'Ray Edwards',
+        username: 'ray',
+        password:
+          '$2a$08$NO9tkFLCoSqX1c5wk3s7z.JfxaVMKA.m7zUDdDwEquo4rvzimQeJm', // 111
         picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
       },
       {
         id: '2',
         name: 'Ethan Gonzalez',
+        username: 'ethan',
+        password:
+          '$2a$08$xE4FuCi/ifxjL2S8CzKAmuKLwv18ktksSN.F3XYEnpmcKtpbpeZgO', // 222
         picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
       },
       {
         id: '3',
         name: 'Bryan Wallace',
+        username: 'bryan',
+        password:
+          '$2a$08$UHgH7J8G6z1mGQn2qx2kdeWv0jvgHItyAsL9hpEUI3KJmhVW5Q1d.', // 333
         picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
       },
       {
         id: '4',
         name: 'Avery Stewart',
+        username: 'avery',
+        password:
+          '$2a$08$wR1k5Q3T9FC7fUgB7Gdb9Os/GV7dGBBf4PLlWT7HERMFhmFDt47xi', // 444
         picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
       },
       {
         id: '5',
         name: 'Katie Peterson',
+        username: 'katie',
+        password:
+          '$2a$08$6.mbXqsDX82ZZ7q5d8Osb..JrGSsNp4R3IKj7mxgF6YGT0OmMw242', // 555
         picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
       },
     ]
