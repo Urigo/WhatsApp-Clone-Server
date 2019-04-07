2e23ba41d323b63c0e498784c3137b97b2b7b424
DAB0mB <emanor6@gmail.com>
2019-04-07 16:54:44 +0800

    Step 10.3: Get current user from auth token

--- index.ts
+++ index.ts
@@ -1,18 +1,27 @@
 import { ApolloServer, gql, PubSub } from 'apollo-server-express'
 import http from 'http'
+import jwt from 'jsonwebtoken'
 import { app } from './app'
 import { users } from './db'
-import { origin, port } from './env'
+import { origin, port, secret } from './env'
 import schema from './schema'
 
 const pubsub = new PubSub()
 const server = new ApolloServer({
   schema,
-  context: ({ req, res }) => ({
-    currentUser: users.find(u => u.id === req.cookies.currentUserId),
-    pubsub,
-    res,
-  }),
+  context: ({ req, res }) => {
+    let currentUser;
+    if (req.cookies.authToken) {
+      const username = jwt.verify(req.cookies.authToken, secret) as string
+      currentUser = username && users.find(u => u.username === username)
+    }
+
+    return {
+      currentUser,
+      pubsub,
+      res,
+    }
+  },
 })
 
 server.applyMiddleware({
