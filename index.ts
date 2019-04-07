d2c06f563bf9357237240c7127f717ac9b10300b
DAB0mB <emanor6@gmail.com>
2019-04-07 16:43:01 +0800

    Step 10.1: Separate app into a different module

--- index.ts
+++ index.ts
@@ -1,22 +1,10 @@
 import { ApolloServer, gql, PubSub } from 'apollo-server-express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import cookie from 'cookie';
-import express from 'express';
 import http from 'http';
+import { app } from './app';
 import { users } from './db';
+import { origin, port } from './env';
 import schema from './schema';
-
-const app = express();
-
-const origin = process.env.ORIGIN || 'http://localhost:3000';
-app.use(cors({ credentials: true, origin }));
-app.use(express.json());
-app.use(cookieParser());
-
-app.get('/_ping', (req, res) => {
-  res.send('pong');
-});
 
 const pubsub = new PubSub();
 const server = new ApolloServer({
@@ -55,8 +43,6 @@ server.applyMiddleware({
 
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
-
-const port = process.env.PORT || 4000;
 
 httpServer.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
