9fe31bc6fec7607d32a890c7d9d2b145a0b3560f
DAB0mB <emanor6@gmail.com>
2019-04-07 16:51:34 +0800

    Step 10.2: Add signIn mutation

--- context.ts
+++ context.ts
@@ -1,7 +1,9 @@
 import { PubSub } from 'apollo-server-express';
 import { User } from './db';
+import { Response } from 'express';
 
 export type MyContext = {
   pubsub: PubSub;
   currentUser: User;
+  res: Response;
 };
