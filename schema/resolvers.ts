b2d4c5174f2f5382ed730f38c239a87e5db0afae
DAB0mB <emanor6@gmail.com>
2019-04-09 14:53:20 +0800

    Step 9.5: Add Subscription.chatRemoved

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -160,7 +160,7 @@ const resolvers: Resolvers = {
       return chat;
     },
 
-    removeChat(root, { chatId }, { currentUser }) {
+    removeChat(root, { chatId }, { currentUser, pubsub }) {
       if (!currentUser) return null;
 
       const chatIndex = chats.findIndex(c => c.id === chatId);
@@ -180,6 +180,11 @@ const resolvers: Resolvers = {
       });
 
       chats.splice(chatIndex, 1);
+
+      pubsub.publish('chatRemoved', {
+        chatRemoved: chat.id,
+        targetChat: chat,
+      });
 
       return chatId;
     },
@@ -206,6 +211,17 @@ const resolvers: Resolvers = {
           if (!currentUser) return false;
 
           return chatAdded.participants.some(p => p === currentUser.id);
+        }
+      ),
+    },
+
+    chatRemoved: {
+      subscribe: withFilter(
+        (root, args, { pubsub }) => pubsub.asyncIterator('chatRemoved'),
+        ({ targetChat }: { targetChat: Chat }, args, { currentUser }) => {
+          if (!currentUser) return false;
+
+          return targetChat.participants.some(p => p === currentUser.id);
         }
       ),
     },
