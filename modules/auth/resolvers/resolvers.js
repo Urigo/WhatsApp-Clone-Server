2d8b9cc406126cfaf963e2607f6f74c17e645c69
Agusti Fernandez <me@agusti.me>
2019-04-26 16:07:37 +0200

    Deleting js build files

--- modules/auth/resolvers/resolvers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_iso_date_1 = require("graphql-iso-date");
-exports.default = {
-    Date: graphql_iso_date_1.GraphQLDateTime,
-};
--- schema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const app_module_1 = require("./modules/app.module");
-exports.default = app_module_1.AppModule.forRoot({}).typeDefs;
