d7293efae638cef30769122eb3d683e3a2877b7f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-05-08 02:23:35 -0400

    Step 11.3: Add Database Client to GraphQL Context

--- context.ts
+++ context.ts
@@ -1,9 +1,11 @@
 import { PubSub } from 'apollo-server-express';
 import { User } from './db';
 import { Response } from 'express';
+import { PoolClient } from 'pg';
 
 export type MyContext = {
   pubsub: PubSub;
   currentUser: User;
   res: Response;
+  db: PoolClient;
 };
