164fa1982b7d525b65aa8e92b8d0c34057b51416
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-05-08 03:37:11 -0400

    Step 11.8: Updating Tests with SQL

--- tests/queries/getUsers.test.ts
+++ tests/queries/getUsers.test.ts
@@ -1,15 +1,27 @@
 import { createTestClient } from 'apollo-server-testing';
 import { ApolloServer, gql } from 'apollo-server-express';
 import schema from '../../schema';
-import { users } from '../../db';
+import { pool } from '../../db';
+import sql from 'sql-template-strings';
+import { MyContext } from '../../context';
 
 describe('Query.getUsers', () => {
   it('should fetch all users except the one signed-in', async () => {
-    let currentUser = users[0];
-
+    const firstUserQuery = await pool.query(
+      sql`SELECT * FROM users WHERE id = 1`
+    );
+    let currentUser = firstUserQuery.rows[0];
+    const db = await pool.connect();
     const server = new ApolloServer({
       schema,
-      context: () => ({ currentUser }),
+      context: async () => ({
+        currentUser,
+        db: await pool.connect(),
+      }),
+      formatResponse: (res: any, { context }: { context: MyContext }) => {
+        context.db.release();
+        return res;
+      },
     });
 
     const { query } = createTestClient(server);
@@ -30,7 +42,10 @@ describe('Query.getUsers', () => {
     expect(res.errors).toBeUndefined();
     expect(res.data).toMatchSnapshot();
 
-    currentUser = users[1];
+    const secondUserQuery = await pool.query(
+      sql`SELECT * FROM users WHERE id = '2'`
+    );
+    currentUser = secondUserQuery.rows[0];
 
     res = await query({
       query: gql`
