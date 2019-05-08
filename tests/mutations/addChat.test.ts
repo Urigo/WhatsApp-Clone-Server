43a67d98673f7feb00e0cac9786bb79cc4f5af07
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-05-08 03:37:11 -0400

    Step 11.8: Updating Tests with SQL

--- tests/mutations/addChat.test.ts
+++ tests/mutations/addChat.test.ts
@@ -1,18 +1,27 @@
 import { createTestClient } from 'apollo-server-testing';
 import { ApolloServer, PubSub, gql } from 'apollo-server-express';
 import schema from '../../schema';
-import { resetDb, users } from '../../db';
+import { resetDb, pool } from '../../db';
+import sql from 'sql-template-strings';
+import { MyContext } from '../../context';
 
 describe('Mutation.addChat', () => {
   beforeEach(resetDb);
 
   it('creates a new chat between current user and specified recipient', async () => {
+    const { rows } = await pool.query(sql`SELECT * FROM users WHERE id = 2`);
+    const currentUser = rows[0];
     const server = new ApolloServer({
       schema,
-      context: () => ({
+      context: async () => ({
         pubsub: new PubSub(),
-        currentUser: users[1],
+        currentUser,
+        db: await pool.connect(),
       }),
+      formatResponse: (res: any, { context }: { context: MyContext }) => {
+        context.db.release();
+        return res;
+      },
     });
 
     const { query, mutate } = createTestClient(server);
@@ -57,12 +66,19 @@ describe('Mutation.addChat', () => {
   });
 
   it('returns the existing chat if so', async () => {
+    const { rows } = await pool.query(sql`SELECT * FROM users WHERE id = 1`);
+    const currentUser = rows[0];
     const server = new ApolloServer({
       schema,
-      context: () => ({
+      context: async () => ({
         pubsub: new PubSub(),
-        currentUser: users[0],
+        currentUser,
+        db: await pool.connect(),
       }),
+      formatResponse: (res: any, { context }: { context: MyContext }) => {
+        context.db.release();
+        return res;
+      },
     });
 
     const { query, mutate } = createTestClient(server);
