3e77d782c58da963a7cf4939fd820e8cf47119d9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-05-08 02:23:35 -0400

    Step 11.3: Add Database Client to GraphQL Context

--- index.ts
+++ index.ts
@@ -2,26 +2,42 @@ import { ApolloServer, gql, PubSub } from 'apollo-server-express'
 import http from 'http'
 import jwt from 'jsonwebtoken'
 import { app } from './app'
-import { users } from './db'
+import { pool } from './db'
 import { origin, port, secret } from './env'
 import schema from './schema'
+import { MyContext } from './context';
+import sql from 'sql-template-strings'
 
 const pubsub = new PubSub()
 const server = new ApolloServer({
   schema,
-  context: ({ req, res }) => {
-    let currentUser;
+  context: async ({ req, res, connection }: any) => {
+    let db;
+
+    if(!connection) {
+      db = await pool.connect();
+    }
+    
+    let currentUser
     if (req.cookies.authToken) {
       const username = jwt.verify(req.cookies.authToken, secret) as string
-      currentUser = username && users.find(u => u.username === username)
+      if (username) {
+        const { rows } =  await pool.query(sql`SELECT * FROM users WHERE username = ${username}`)
+        currentUser = rows[0]
+      }
     }
-
     return {
       currentUser,
       pubsub,
       res,
+      db,
     }
   },
+  formatResponse: (res: any, { context }: { context: MyContext }) => {
+    context.db.release()
+
+    return res
+  }
 })
 
 server.applyMiddleware({
