272bf16fed9585ed8098416fb71f05a853937e6f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2019-05-13 16:57:10 +0200

    Step 12.4: Use Apollo DataSources

--- context.ts
+++ context.ts
@@ -2,10 +2,14 @@ import { PubSub } from 'apollo-server-express';
 import { User } from './db';
 import { Response } from 'express';
 import { PoolClient } from 'pg';
+import { UnsplashApi } from './schema/unsplash.api';
 
 export type MyContext = {
   pubsub: PubSub;
   currentUser: User;
   res: Response;
   db: PoolClient;
+  dataSources: {
+    unsplashApi: UnsplashApi;
+  };
 };
