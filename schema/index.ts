afa13f92f502562b098153d971f84474d3a36d2c
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-14 10:40:57 +0200

    Step 13.1: Modularize schema

--- schema/index.ts
+++ schema/index.ts
@@ -1,10 +1,15 @@
-import { importSchema } from 'graphql-import';
 import { makeExecutableSchema, IResolvers } from 'graphql-tools';
-import resolvers from './resolvers';
-
-const typeDefs = importSchema('schema/typeDefs.graphql');
+import { merge } from 'lodash';
+import * as commonModule from '../modules/common';
+import * as usersModule from '../modules/users';
+import * as chatsModule from '../modules/chats';
 
 export default makeExecutableSchema({
-  resolvers: resolvers as IResolvers,
-  typeDefs,
+  resolvers: merge(
+    {},
+    commonModule.resolvers,
+    usersModule.resolvers,
+    chatsModule.resolvers
+  ) as IResolvers,
+  typeDefs: [commonModule.typeDefs, usersModule.typeDefs, chatsModule.typeDefs],
 });
