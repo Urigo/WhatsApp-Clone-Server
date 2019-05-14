f2520de0d65c4e8015c9d97a42604fc5a78cf3b3
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-14 13:59:56 +0200

    Step 13.5: Use Dependency Injection

--- modules/common/index.ts
+++ modules/common/index.ts
@@ -7,7 +7,7 @@ import { Resolvers } from '../../types/graphql';
 const { PostgresPubSub } = require('graphql-postgres-subscriptions');
 
 const typeDefs = gql`
-  scalar Date
+  scalar DateTime
 
   type Query {
     _dummy: Boolean
@@ -23,7 +23,7 @@ const typeDefs = gql`
 `;
 
 const resolvers: Resolvers = {
-  Date: GraphQLDateTime,
+  DateTime: GraphQLDateTime,
 };
 
 const pubsub = new PostgresPubSub({
