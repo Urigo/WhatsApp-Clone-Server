08626c715808443d2ae32f4849d9fd8982b77dd1
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-14 13:59:56 +0200

    Step 13.5: Use Dependency Injection

--- context.ts
+++ context.ts
@@ -1,13 +1,12 @@
 import { PubSub } from 'apollo-server-express';
+import { ModuleContext } from '@graphql-modules/core';
 import { User } from './db';
 import { Response } from 'express';
 import { PoolClient } from 'pg';
-import { UnsplashApi } from './modules/chats/unsplash.api';
 
 export type MyContext = {
   pubsub: PubSub;
   currentUser: User;
   res: Response;
   db: PoolClient;
-  unsplashApi: UnsplashApi;
-};
+} & ModuleContext;
