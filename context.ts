ef1e96c7ef3c95fd20e1926a67f2f3da6cb0cf2b
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-14 12:07:13 +0200

    Step 13.4: Migrate RESTDataSource

--- context.ts
+++ context.ts
@@ -2,14 +2,12 @@ import { PubSub } from 'apollo-server-express';
 import { User } from './db';
 import { Response } from 'express';
 import { PoolClient } from 'pg';
-import { UnsplashApi } from './schema/unsplash.api';
+import { UnsplashApi } from './modules/chats/unsplash.api';
 
 export type MyContext = {
   pubsub: PubSub;
   currentUser: User;
   res: Response;
   db: PoolClient;
-  dataSources: {
-    unsplashApi: UnsplashApi;
-  };
+  unsplashApi: UnsplashApi;
 };
