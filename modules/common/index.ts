20583e0491fe551fb8dee280b9c60000ef4f806c
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-14 11:05:59 +0200

    Step 13.3: Use GraphQL Modules

--- modules/common/index.ts
+++ modules/common/index.ts
@@ -1,8 +1,12 @@
+import { GraphQLModule } from '@graphql-modules/core';
 import { gql } from 'apollo-server-express';
 import { GraphQLDateTime } from 'graphql-iso-date';
+import { pool } from '../../db';
 import { Resolvers } from '../../types/graphql';
 
-export const typeDefs = gql`
+const { PostgresPubSub } = require('graphql-postgres-subscriptions');
+
+const typeDefs = gql`
   scalar Date
 
   type Query {
@@ -18,6 +22,33 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers: Resolvers = {
+const resolvers: Resolvers = {
   Date: GraphQLDateTime,
 };
+
+const pubsub = new PostgresPubSub({
+  host: 'localhost',
+  port: 5432,
+  user: 'testuser',
+  password: 'testpassword',
+  database: 'whatsapp',
+});
+
+export default new GraphQLModule({
+  name: 'common',
+  typeDefs,
+  resolvers,
+  async context({ res, connection }) {
+    let db;
+
+    if (!connection) {
+      db = await pool.connect();
+    }
+
+    return {
+      pubsub,
+      res,
+      db,
+    };
+  },
+});
