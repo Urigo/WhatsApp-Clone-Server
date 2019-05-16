0bd9522d1f203a77c31edf9f3ff0791dd7105ae0
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-16 17:53:18 +0200

    Step 13.27: Adjust tests

--- tests/mutations/removeChat.test.ts
+++ tests/mutations/removeChat.test.ts
@@ -1,28 +1,14 @@
 import { createTestClient } from 'apollo-server-testing';
-import { ApolloServer, PubSub, gql } from 'apollo-server-express';
-import { rootModule } from '../../index';
-import { resetDb, pool } from '../../db';
-import sql from 'sql-template-strings';
-import { MyContext } from '../../context';
+import { gql } from 'apollo-server-express';
+import { server } from '../../server';
+import { resetDb } from '../../db';
+import { mockAuth } from '../mocks/auth.provider';
 
 describe('Mutation.removeChat', () => {
   beforeEach(resetDb);
 
   it('removes chat by id', async () => {
-    const { rows } = await pool.query(sql`SELECT * FROM users WHERE id = 1`);
-    const currentUser = rows[0];
-    const server = new ApolloServer({
-      schema: rootModule.schema,
-      context: async () => ({
-        pubsub: new PubSub(),
-        currentUser,
-        db: await pool.connect(),
-      }),
-      formatResponse: (res: any, { context }: { context: MyContext }) => {
-        context.db.release();
-        return res;
-      },
-    });
+    mockAuth(1);
 
     const { query, mutate } = createTestClient(server);
 
