ed9c07d66ecffd65b9bc39d2d69c3c5d08acd359
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-16 17:20:55 +0200

    Step 13.20: Implement Auth provider with currentUser method

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import { GraphQLModule } from '@graphql-modules/core';
+import cookie from 'cookie';
 import http from 'http';
 import { app } from './app';
 import { origin, port } from './env';
@@ -15,7 +16,18 @@ export const rootModule = new GraphQLModule({
 
 const server = new ApolloServer({
   schema: rootModule.schema,
-  context: rootModule.context,
+  context: (session: any) => {
+    if (session.connection) {
+      const req = session.connection.context.session.request;
+      const cookies = req.headers.cookie;
+
+      if (cookies) {
+        req.cookies = cookie.parse(cookies);
+      }
+    }
+
+    return rootModule.context(session);
+  },
   subscriptions: rootModule.subscriptions,
 });
 
