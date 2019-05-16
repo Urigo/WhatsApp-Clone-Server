aa0f33c2d97b587156aa7559dd50e5d794e048d6
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-16 17:24:50 +0200

    Step 13.21: Remove db from context

--- context.ts
+++ context.ts
@@ -1,8 +1,6 @@
 import { ModuleContext } from '@graphql-modules/core';
 import { Response } from 'express';
-import { PoolClient } from 'pg';
 
 export type MyContext = {
   res: Response;
-  db: PoolClient;
 } & ModuleContext;
