24af1bdb387a8b9b34a5c317fd87d45cb3e5922c
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-16 17:26:27 +0200

    Step 13.22: Move signUp logic to Auth provider

--- modules/users/index.ts
+++ modules/users/index.ts
@@ -4,7 +4,6 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import commonModule from '../common';
 import { secret, expiration } from '../../env';
-import { validateLength, validatePassword } from '../../validators';
 import { Resolvers } from '../../types/graphql';
 import { Users } from './users.provider';
 import { Auth } from './auth.provider';
@@ -67,26 +66,7 @@ const resolvers: Resolvers = {
     },
 
     async signUp(root, { name, username, password, passwordConfirm }, { injector }) {
-      validateLength('req.name', name, 3, 50);
-      validateLength('req.username', name, 3, 18);
-      validatePassword('req.password', password);
-
-      if (password !== passwordConfirm) {
-        throw Error("req.password and req.passwordConfirm don't match");
-      }
-
-      const existingUser = await injector.get(Users).findByUsername(username);
-      if (existingUser) {
-        throw Error('username already exists');
-      }
-
-      const createdUser = await injector.get(Users).newUser({
-        username,
-        password,
-        name,
-      });
-
-      return createdUser;
+      return injector.get(Auth).signUp({name, username, password, passwordConfirm });
     },
   },
 };
