4e9ee3f42b3875282256a00312c3b486a86caa13
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-05-16 17:26:27 +0200

    Step 13.22: Move signUp logic to Auth provider

--- modules/users/auth.provider.ts
+++ modules/users/auth.provider.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject, ProviderScope } from '@graphql-modules/di';
 import { ModuleSessionInfo } from '@graphql-modules/core';
 import jwt from 'jsonwebtoken';
 import { secret } from '../../env';
+import { validateLength, validatePassword } from '../../validators';
 import { Users } from './users.provider';
 import { User } from '../../db';
 
@@ -14,6 +15,38 @@ export class Auth {
 
   private get req() {
     return this.module.session.req || this.module.session.request;
+  }
+
+  async signUp({
+    name,
+    password,
+    passwordConfirm,
+    username,
+  }: {
+    name: string;
+    password: string;
+    passwordConfirm: string;
+    username: string;
+  }) {
+    validateLength('req.name', name, 3, 50);
+    validateLength('req.username', username, 3, 18);
+    validatePassword('req.password', password);
+
+    if (password !== passwordConfirm) {
+      throw Error("req.password and req.passwordConfirm don't match");
+    }
+
+    const existingUser = await this.users.findByUsername(username);
+
+    if (existingUser) {
+      throw Error('username already exists');
+    }
+
+    return this.users.newUser({
+      username,
+      name,
+      password,
+    });
   }
 
   async currentUser(): Promise<User | null> {
