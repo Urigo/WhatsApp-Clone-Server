a7f4a9357161b7586abacbfd266ffa053ad8d91b
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-06-03 10:25:18 +0200

    Step 14.9: Add fake data

--- env.ts
+++ env.ts
@@ -5,3 +5,6 @@ export const secret = process.env.JWT_SECRET || '70p53cr37';
 export const origin = process.env.ORIGIN || 'http://localhost:3000';
 export const port = process.env.PORT || 4000;
 export const resetDb = process.env.RESET_DB || false;
+export const fakedDb = process.env.FAKED_DB
+  ? parseInt(process.env.FAKED_DB, 10)
+  : 0;
